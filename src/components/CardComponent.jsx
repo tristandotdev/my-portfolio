5db2697bdc11cff235cfe1af8b8a456ef60583b9
tristan <tristandepoosingh001@gmail.com>
2026-08-14 15:30:07 -0400

    Finished Card Component

--- src/components/CardComponent.jsx
+++ src/components/CardComponent.jsx
@@ -1,6 +1,20 @@
 import './Cards.css'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function CardComponent({ name , gitUrl , desc , imageUrl }) {
+  
+  const [mobileMode, setMobileMode] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setMobileMode(window.innerWidth < 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return(
     <>
       <div className="cardDiv">
@@ -9,12 +23,20 @@ function CardComponent({ name , gitUrl , desc , imageUrl }) {
           justifyContent: "space-between",
           alignItems: "center",
           padding: "0",
-          margin: "0"
+          margin: "0",
+          marginBottom: "10px"
         }}>
-          <h1>{name}</h1>
-          <a href={gitUrl}><img style={{width: "32px"}} src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub Link" /></a>
+          <h1 style={{fontSize: !mobileMode ? "42px" : "24px", margin: "0px"}}>{name}</h1>
+          <a 
+            target="_blank" 
+            href={gitUrl}>
+            <img 
+              style={{width: "36px"}} 
+              src="https://cdn-icons-png.flaticon.com/512/25/25231.png" 
+              alt="GitHub Link" />
+          </a>
         </div>
-        {imageUrl && <img src={imageUrl}></img>}
+        {imageUrl && <img style={{width: "100%"}} src={imageUrl}></img>}
         <p>{desc}</p>
       </div>
     </>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,9 +6,10 @@ function Projects() {
       <h1 style={{marginBottom: "50px"}}>Projects</h1>
       <div className="verticalDiv" style={{maxWidth: "80%"}}>
         <CardComponent
-          name="John"
-          gitUrl="fioeansduoifhsf"
-          desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, nisi deleniti suscipit ab sed omnis sit blanditiis consequuntur accusamus dignissimos amet, harum error qui quisquam voluptatem at officia quo impedit."
+          name="Schedule Scraper"
+          gitUrl="https://github.com/tristandotdev/Scheduler"
+          desc="Uses PDF.js to extract course data from the UWI timetables website"
+          imageUrl=""
         />
         <CardComponent/>
       </div>
