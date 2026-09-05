e604a8932568f0273ba433baadb410adcd7f928c
tristan <tristandepoosingh001@gmail.com>
2026-09-05 14:04:21 -0400

    added portfolio to projects

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,7 +4,7 @@ function Projects() {
     <>
     <div className="horizontalDiv" id="Projects" style={{height: "75vh"}}>
       <h1 style={{marginBottom: "50px"}}>Projects</h1>
-      <div className="verticalDiv" style={{maxWidth: "80%"}}>
+      <div className="verticalDiv" id="project-cards">
         <CardComponent
           name="Schedule Scraper"
           gitUrl="https://github.com/tristandotdev/Scheduler"
@@ -18,7 +18,13 @@ function Projects() {
           youtubeUrl="https://youtu.be/998j2pK5Ews"
           desc="Allows users to create their own Deadlock builds utilizing Firebase as a backend."
           imageUrl=""
-          tech="HTML/CSS, JavaScript, Firebase Auth"
+          tech="HTML/CSS, JavaScript, Firebase Authentication"
+        />
+        <CardComponent
+          name="Portfolio Site"
+          gitUrl="https://github.com/tristandotdev/my-portfolio"
+          desc="What you're currently on! Built from scratch using React."
+          tech="HTML/CSS, React"
         />
       </div>
     </div>
